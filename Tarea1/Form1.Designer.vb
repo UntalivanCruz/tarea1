99830ab59dd6212178c6d1fb53b61425996d15db
Ivan Cruz <ivancruzhn@gmail.com>
2021-04-23 18:29:16 -0600

    Primera de la tarea

---
+++ Tarea1/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtSegundoValor = New System.Windows.Forms.TextBox()
+        Me.txtPrimerValor = New System.Windows.Forms.TextBox()
+        Me.cmbOperacion = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtResultado = New System.Windows.Forms.TextBox()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtSegundoValor)
+        Me.GroupBox1.Controls.Add(Me.txtPrimerValor)
+        Me.GroupBox1.Controls.Add(Me.cmbOperacion)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(25, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(331, 129)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Ingreso de datos"
+        '
+        'txtSegundoValor
+        '
+        Me.txtSegundoValor.Location = New System.Drawing.Point(124, 93)
+        Me.txtSegundoValor.Name = "txtSegundoValor"
+        Me.txtSegundoValor.Size = New System.Drawing.Size(184, 23)
+        Me.txtSegundoValor.TabIndex = 6
+        '
+        'txtPrimerValor
+        '
+        Me.txtPrimerValor.Location = New System.Drawing.Point(124, 58)
+        Me.txtPrimerValor.Name = "txtPrimerValor"
+        Me.txtPrimerValor.Size = New System.Drawing.Size(184, 23)
+        Me.txtPrimerValor.TabIndex = 5
+        '
+        'cmbOperacion
+        '
+        Me.cmbOperacion.FormattingEnabled = True
+        Me.cmbOperacion.Items.AddRange(New Object() {"Suma", "Resta", "Multiplicacion", "Division", "Raiz Cuadrada", "Exponencial"})
+        Me.cmbOperacion.Location = New System.Drawing.Point(124, 24)
+        Me.cmbOperacion.Name = "cmbOperacion"
+        Me.cmbOperacion.Size = New System.Drawing.Size(184, 23)
+        Me.cmbOperacion.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(10, 101)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(86, 15)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Segundo Valor:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(10, 66)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(74, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Primer Valor:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(10, 32)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Operacion:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(35, 204)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(90, 15)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "Tu resultado es:"
+        '
+        'txtResultado
+        '
+        Me.txtResultado.Location = New System.Drawing.Point(149, 196)
+        Me.txtResultado.Name = "txtResultado"
+        Me.txtResultado.ReadOnly = True
+        Me.txtResultado.Size = New System.Drawing.Size(184, 23)
+        Me.txtResultado.TabIndex = 6
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(97, 149)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(164, 36)
+        Me.btnCalcular.TabIndex = 7
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(368, 231)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.txtResultado)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Operaciones Aritmeticas"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtSegundoValor As TextBox
+    Friend WithEvents txtPrimerValor As TextBox
+    Friend WithEvents cmbOperacion As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtResultado As TextBox
+    Friend WithEvents btnCalcular As Button
+End Class
---
+++ Tarea1/Form1.vb
@@ -0,0 +1,59 @@
+﻿Public Class Form1
+    Private Function getSuma(primerValor As Integer, segundoValor As Integer) As Integer
+        Dim resultado As Integer = primerValor + segundoValor
+        Return resultado
+    End Function
+
+    Private Function getResta(primerValor As Integer, segundoValor As Integer) As Integer
+        Dim resultado As Integer = primerValor - segundoValor
+        Return resultado
+    End Function
+
+    Private Function getMultiplicacion(primerValor As Integer, segundoValor As Integer) As Integer
+        Dim resultado As Integer = primerValor * segundoValor
+        Return resultado
+    End Function
+    Private Function getDivision(primerValor As Integer, segundoValor As Integer) As Integer
+        Dim resultado As Integer = primerValor / segundoValor
+        Return resultado
+    End Function
+    Private Function getRaiz(primerValor As Integer, segundoValor As Integer) As Integer
+        Dim resultado As Integer = Math.Sqrt(primerValor) + Math.Sqrt(segundoValor)
+        Return resultado
+    End Function
+    Private Function getExponente(primerValor As Integer, segundoValor As Integer) As Integer
+        Dim resultado As Integer = primerValor ^ segundoValor
+        Return resultado
+    End Function
+
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Dim resultado As Integer
+        Select Case cmbOperacion.SelectedIndex
+            Case 0
+                resultado = getSuma(txtPrimerValor.Text, txtSegundoValor.Text)
+            Case 1
+                resultado = getResta(txtPrimerValor.Text, txtSegundoValor.Text)
+            Case 2
+                resultado = getMultiplicacion(txtPrimerValor.Text, txtSegundoValor.Text)
+            Case 3
+                resultado = getDivision(txtPrimerValor.Text, txtSegundoValor.Text)
+            Case 4
+                resultado = getRaiz(txtPrimerValor.Text, txtSegundoValor.Text)
+            Case 5
+                resultado = getExponente(txtPrimerValor.Text, txtSegundoValor.Text)
+        End Select
+        txtResultado.Text = resultado
+    End Sub
+
+    Private Sub txtPrimerValor_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtPrimerValor.KeyPress
+        If Not IsNumeric(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtSegundoValor_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtSegundoValor.KeyPress
+        If Not IsNumeric(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+End Class
